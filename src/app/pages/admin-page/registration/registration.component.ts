03da38060d97822362309698d77f236844f3a627
Stanuslav Pilguy <stanislav.pilguy@gmail.com>
2022-02-20 09:02:36 +0200

    page registration

--- src/app/pages/admin-page/registration/registration.component.ts
+++ src/app/pages/admin-page/registration/registration.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
 
