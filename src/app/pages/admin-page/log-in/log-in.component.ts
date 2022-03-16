a39dad094bd5ed95465ebcba6324a27754bc6b13
Stanislav Pilguy <stanislav.pilguy@gmail.com>
2022-03-16 20:25:30 +0200

    add images

--- src/app/pages/admin-page/log-in/log-in.component.ts
+++ src/app/pages/admin-page/log-in/log-in.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
 })
 export class LogInComponent implements OnInit {
+  public form!: FormGroup;
 
-  constructor() { }
+  constructor(
+    // private authService: AuthService,
+    // private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.min(6)
+      ])
+    })
   }
 
+  onSubmit() {
+
+  }
 }
