50cf1bbf08079dcd1da055638ae3895d61f281b9
Stanislav Pilguy <stanislav.pilguy@gmail.com>
2022-03-20 17:42:07 +0200

    add apiService

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  readonly url = environment.url;
 
   constructor() { }
 }
