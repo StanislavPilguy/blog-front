a8772d601dbabd4d43cd4404f9a923b21559eb47
Stanislav Pilguy <stanislav.pilguy@gmail.com>
2022-03-20 17:57:14 +0200

    add authService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {AdminPageModule} from "./pages/admin-page/admin-page.module";
 import {ComponentsModule} from "./components/components.module";
-
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -22,8 +22,10 @@ import {ComponentsModule} from "./components/components.module";
     ComponentsModule,
     ReactiveFormsModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+
+import {environment} from "../../environments/environment";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  readonly url = environment.url;
+  readonly singIn = this.url + 'auth/log-in';
+  readonly register = this.url + 'auth/registration';
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _router: Router
+  ) {}
+
+
 }
