11a57584b0753b2b9555b919ef8fa0872ae0ab12
Stanislav <stanislav.pilguy@gmail.com>
2022-04-16 17:28:04 +0300

    dashboard-style

--- src/app/pages/admin-page/pages/dashboard-page/dashboard-page-routing.module.ts
+++ src/app/pages/admin-page/pages/dashboard-page/dashboard-page-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardPageComponent} from "./dashboard-page.component";
-import {CategoriesComponentComponent} from "./components/categories-component/categories-component.component";
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardPageComponent, children: [
-      {path: '', pathMatch: 'full', redirectTo: 'categories'},
-      {
-        path: 'categories',
-        component: CategoriesComponentComponent
-      }
+      {path: '', pathMatch: 'full', redirectTo: 'admin/dashboard'},
     ]
   },
 
--- src/app/pages/admin-page/pages/dashboard-page/dashboard-page.module.ts
+++ src/app/pages/admin-page/pages/dashboard-page/dashboard-page.module.ts
@@ -3,18 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardPageRoutingModule } from './dashboard-page-routing.module';
 import { DashboardPageComponent } from './dashboard-page.component';
-import { CategoriesComponentComponent } from './components/categories-component/categories-component.component';
-
 
 
 @NgModule({
   declarations: [
     DashboardPageComponent,
-    CategoriesComponentComponent,
   ],
-    imports: [
-        CommonModule,
-        DashboardPageRoutingModule,
-    ]
+  imports: [
+    CommonModule,
+    DashboardPageRoutingModule,
+  ]
 })
 export class DashboardPageModule { }
