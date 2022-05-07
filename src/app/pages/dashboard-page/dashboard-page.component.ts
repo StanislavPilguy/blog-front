9c3e6fd544ff69dbefc33c383d760a5974370b31
Stanislav <stanislav.pilguy@gmail.com>
2022-05-07 18:26:56 +0300

    added left menu

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {AdminCategoriesService} from "../../services/admin-categories.service";
+import {IAdminCategories} from "../../interfaces/iAdmin-categories";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
 })
 export class DashboardPageComponent implements OnInit {
+  public categoriesAdmin$!: Observable<IAdminCategories[]>;
   isShow: boolean = false;
   isContentShow: boolean = false;
 
-  constructor() { }
+  constructor(
+    private admin: AdminCategoriesService
+  ) { }
 
   ngOnInit(): void {
+    this.categoriesAdmin$ = this.admin.getAdminCategories()
   }
 
   toggleClass() {
