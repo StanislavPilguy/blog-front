9ba15a892522f907bb742022da3ba2128e97a523
Stanislav <stanislav.pilguy@gmail.com>
2022-05-14 12:17:45 +0300

    added category

--- src/app/pages/dashboard-page/components/category/category.component.ts
+++ src/app/pages/dashboard-page/components/category/category.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
 
